7d1a0515135f13555832c81c306ba95e697ce244
Joe Karasek <joseph.karasek@gmail.com>
2017-06-14 11:35:55 -0700

    init commit, rollover from Langan

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dr. Culberson for Tyson Steele
+
+## Description
+A website for a dentist. See template documentation for further information
+
---
+++ src/js/scripts.js
@@ -0,0 +1,73 @@
+(function($, HelloWorldDevsTysonSteele) {
+
+  // ======= Fixes =======
+  HelloWorldDevsTysonSteele.noOrphans('h1,h2,h3,h4,h5,h6,li,p', '.allow-orphan');
+  HelloWorldDevsTysonSteele.scrollToFix('#primary-menu');
+
+  // ======= Request Appointment Form =======
+  HelloWorldDevsTysonSteele.requestAppointment('#mail-form', '#success_msg' , '7fb35345-752d-4792-9480-cd3db6674a62');
+
+  // ======= Carousels =======
+  HelloWorldDevsTysonSteele.marqueeCarousel({
+    autoplay: 6000,
+    effect: 'fade',
+    speed: 500
+  });
+  HelloWorldDevsTysonSteele.tourCarousel('.js-tour-carousel', {
+    items: 1,
+    margin: 40,
+    navText: [
+      '<i class="icon-chevron-left"></i>',
+      '<i class="icon-chevron-right"></i>'
+    ],
+    responsive: {
+      768: {
+        items: 2
+      }
+    }
+  });
+  HelloWorldDevsTysonSteele.associationCarousel('.js-associations-carousel', {
+    items: 2,
+    loop: true,
+    autoplay: true,
+    autoplaySpeed: 500,
+    autoplayTimeout: 2000,
+    autoplayHoverPause:false,
+    autoWidth: false,
+    nav: false,
+    dots: false,
+    margin: 40,
+    slideBy: 1,
+    responsive: {
+      550: {
+        items: 3
+      },
+      992: {
+        items: 4
+      }
+    }
+  });
+  HelloWorldDevsTysonSteele.pyramidCarousel('.js-team-carousel', {
+    items: 2,
+    navText: [
+      '<img src="assets/team-arrow-left.png">',
+      '<img src="assets/team-arrow-right.png">'
+    ],
+    responsive: {
+      480: {
+        items: 3
+      },
+      768: {
+        items: 4
+      }
+    },
+    margin: 10
+  });
+  HelloWorldDevsTysonSteele.updateCopyright('.js-copyright-year');
+
+  // ======= Google Maps =======
+  HelloWorldDevsTysonSteele.googleMap('#google-map5', '42.0329231,-97.4641569', '42.0329231,-97.4641569');
+
+
+}(jQuery, HelloWorldDevsTysonSteele));
+
